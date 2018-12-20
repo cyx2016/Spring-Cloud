b4b824e27340ff96444e73479d0fcd117087c059
cyx <yxchen@zjft.com>
2018-12-20 20:56:07 +0800

    cloud 组件初建

---
+++ service-ribbon/service-ribbon/src/main/java/com/example/controller/HelloControler.java
@@ -0,0 +1,27 @@
+package com.example.controller;
+
+import com.example.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created by
+ * https://blog.csdn.net/forezp/article/details/69788938
+ */
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+
+
+}
+
+
---
+++ service-ribbon/service-ribbon/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan({"com.example.controller","com.example.service"})
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
+
